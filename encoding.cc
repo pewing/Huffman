b0d5446b6d9d01678fa4f8e13cca704f9877d8a4
Philip Ewing <philip.jh.ewing@gmail.com>
2015-08-11 16:55:17 -0700

    Day 3

--- encoding.cc
+++ encoding.cc
@@ -5,11 +5,14 @@
 using namespace std;
 
 void insert(Node * queue, Hnode * huffnode) {
-	if (!queue)
+	if (!queue) {
 		queue = new Node(huffnode);
-	
-	else if (huffnode->count <= queue->data->count)
+		cout << huffnode->letter << endl;
+	}
+	else if (huffnode->count <= queue->data->count) {
 		queue = new Node(huffnode, queue);
+		cout << huffnode->letter << endl;
+	}
 	else
 		insert(queue->next, huffnode);
 }
@@ -17,9 +20,14 @@ void insert(Node * queue, Hnode * huffnode) {
 void print_tree(Hnode * root, int indent) {
 	if (!root)
 		return;
-	cout << root << 
+	if (!root->left)  //leaf
+		cout << string(indent, ' ') << root->count << root->letter;
+	else {
+		cout << "[]" << endl;
+		print_tree(root->left, indent+1);
+		print_tree(root->right, indent+1);
+	}
 }
-
 
 int main(int argc, char * argv[]) {
 	ifstream readFile;
@@ -29,6 +37,8 @@ int main(int argc, char * argv[]) {
 	int Count[256];
 	for (int i=0; i<256; i++)
 		Count[i] = 0;
+
+	cout << "test |" << string(7, ' ') <<"|" <<endl;
 	
 	//int j=0;
 	while (!readFile.eof()) {
@@ -41,17 +51,37 @@ int main(int argc, char * argv[]) {
 	for (int i=0; i<256; i++) 
 		if (Count[i])
 			insert(queue, new Hnode((unsigned char)i, Count[i]));
-
 		
+	cout << "1" <<endl;
 	// NEED TO CHECK IF ONLY ONE LETTER
-	Hnode * root = new Hnode();
-	root->right = queue->data;
+	Hnode * old_root = new Hnode();
+	cout << "1.5" <<endl;
+	if (!queue)
+		cout << "huh" <<endl;
+	old_root->right = queue->data;
+	
+	cout << "2" <<endl;
+	
 	Node * temp = queue;
 	queue = queue->next;
+	
+	cout << "3" <<endl;
 	delete temp;
 	
+	cout << "2" <<endl;
+	
 	while (queue) {
-		
+		Hnode * root = new Hnode();
+		Hnode * leaf = queue->data;
+		root->left = leaf;
+		root->right = old_root;
+		Node * temp = queue;
+		queue = queue->next;
+		delete temp;
+		old_root = root;
 	}
+	
+	cout << "test |" << string(7, ' ') <<"|" <<endl;
+	
 	return 0;
 }
