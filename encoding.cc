6f59b0c8844e140d20629169348b3c704cb9a31a
Philip Ewing <philip.jh.ewing@gmail.com>
2015-08-12 18:38:18 -0700

    Fixed print

--- encoding.cc
+++ encoding.cc
@@ -4,14 +4,14 @@
 #include "tree.h"
 using namespace std;
 
-void insert(Node * queue, Hnode * huffnode) {
+void insert(Node * & queue, Hnode * huffnode) {
 	if (!queue) {
 		queue = new Node(huffnode);
-		cout << huffnode->letter << endl;
+		//cout << queue->data->letter << endl;
 	}
 	else if (huffnode->count <= queue->data->count) {
 		queue = new Node(huffnode, queue);
-		cout << huffnode->letter << endl;
+		//cout << queue->data->letter << endl;
 	}
 	else
 		insert(queue->next, huffnode);
@@ -21,11 +21,13 @@ void print_tree(Hnode * root, int indent) {
 	if (!root)
 		return;
 	if (!root->left)  //leaf
-		cout << string(indent, ' ') << root->count << root->letter;
+		cout << string(indent-4, ' ') << root->count << root->letter;
 	else {
-		cout << "[]" << endl;
-		print_tree(root->left, indent+1);
-		print_tree(root->right, indent+1);
+		cout << string(3, ' ') << "[]" << endl;
+		cout << string(indent, ' ') << "/";
+		cout << "  \\" << endl;
+		print_tree(root->left, indent+3);
+		print_tree(root->right, indent+3);
 	}
 }
 
@@ -38,7 +40,7 @@ int main(int argc, char * argv[]) {
 	for (int i=0; i<256; i++)
 		Count[i] = 0;
 
-	cout << "test |" << string(7, ' ') <<"|" <<endl;
+	//cout << "test |" << string(7, ' ') <<"|" <<endl;
 	
 	//int j=0;
 	while (!readFile.eof()) {
@@ -52,26 +54,20 @@ int main(int argc, char * argv[]) {
 		if (Count[i])
 			insert(queue, new Hnode((unsigned char)i, Count[i]));
 		
-	cout << "1" <<endl;
+	
 	// NEED TO CHECK IF ONLY ONE LETTER
 	Hnode * old_root = new Hnode();
-	cout << "1.5" <<endl;
-	if (!queue)
-		cout << "huh" <<endl;
 	old_root->right = queue->data;
-	
-	cout << "2" <<endl;
-	
+		
 	Node * temp = queue;
 	queue = queue->next;
-	
-	cout << "3" <<endl;
+
 	delete temp;
-	
-	cout << "2" <<endl;
-	
+		
+	Hnode * root = NULL;
+		
 	while (queue) {
-		Hnode * root = new Hnode();
+		root = new Hnode();
 		Hnode * leaf = queue->data;
 		root->left = leaf;
 		root->right = old_root;
@@ -81,7 +77,8 @@ int main(int argc, char * argv[]) {
 		old_root = root;
 	}
 	
-	cout << "test |" << string(7, ' ') <<"|" <<endl;
+	print_tree(root, 2);
+	
 	
 	return 0;
 }
