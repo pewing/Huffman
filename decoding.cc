e3f207f483cf52e3633cdc5814e346886254990c
pewing <philip.jh.ewing@gmail.com>
2016-07-19 19:03:42 -0700

    separate graph path

--- decoding.cc
+++ decoding.cc
@@ -1,6 +1,6 @@
 // Huffman decoder
 // Author: Philip Ewing
-// Start date: September 21, 2015
+
 
 #include <iostream>
 #include <fstream>
@@ -9,58 +9,68 @@
 
 using namespace std;
 
-void form_tree(Hnode * & root, string & str) {
-	if (!root) {
-		if (str[0] == 'O') {
-			root = new Hnode();
-			root->letter = str[1];
-			str = str.substr(2);
-		}
-		else if (str[0] == 'I') {
-			root = new Hnode();
-			str = str.substr(1);
-			form_tree(root->left, str);
-			form_tree(root->right, str);
-		}
-	}
-	
-}
-
-
 int main(int argc, char * argv[]) {
 	if (argc != 2 ) {
 		cout << "ERROR - Proper syntax:  decoder.exe filename.huf"<<endl;
 		return 0;
 	}
-	string file_name = argv[1];
-	int file_name_size = file_name.size();
-	if (file_name_size < 5 || file_name.substr(file_name_size-4) != ".huf") {
+	string filename = argv[1];
+	int filenameSize = filename.size();
+	if (filenameSize < 5 || filename.substr(filenameSize-4) != ".huf") {
 		cout << "ERROR - Can only decode .huf files" <<endl;
-		return 0; 
-	}
-	
-	ifstream readFile(file_name.c_str());
-	int char_count = 0;
-	readFile >> char_count;
-	
-	if (!char_count) {
-		ofstream writeFile((file_name.substr(file_name_size-4) + ".dehuf").c_str());
-		readFile.close(); 
-		writeFile.close();
 		return 0;
 	}
-	
-	char c;
-	while (1) {
-		readFile.get(c);
-		if (not readFile)
-			break;
-		cout << c;
+
+	ifstream iFile(filename);
+	ofstream oFole(filename.substr(0, filenameSize - 4) + ".txt");
+
+	if (!iFile.is_open()) {
+		cout << "ERROR - Unable to open file" << endl;
+		return 0;
 	}
-	cout << endl;
-	Hnode * root = NULL;
-	string str = "IIIIO\nO1OsOtIO3IOeO ";
-	form_tree(root, str);
-	
+
+	string treePath = "";
+	char ch;
+	int l = 0;
+	int r = 0;
+	int c = 0;
+	bool doneTreePath = false;
+	Hnode * top = NULL;
+
+	while (iFile.get(ch)) {
+		if (!doneTreePath) {
+			switch (ch) {
+				case 'L':
+					l++;
+					break;
+				case 'R':
+					r++;
+					break;
+				case 'C':
+					c++;
+					treePath += ch;
+					iFile.get(ch);
+					break;
+				default:
+					cout << "ERROR - Graph not properly formatted";
+					cout<< l<<r<<c;
+					return 0;
+			}
+			treePath += ch;
+			if (l == r && c == l+1) {
+					doneTreePath = true;
+					cout << treePath;
+			}
+		}
+		else {
+
+		}
+	}
+
+
+	// bool postC;
+
+
+
 	return 0;
 }
